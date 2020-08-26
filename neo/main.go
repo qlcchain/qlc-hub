624a6a6012853d40c1b8bffc10c71b2c4f0e838e
zengchen221 <931891076@qq.com>
2020-08-26 17:14:46 +0800

    feat: update str

--- neo/main.go
+++ neo/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/nspcc-dev/neo-go/pkg/core/transaction"
+	"github.com/nspcc-dev/neo-go/pkg/encoding/address"
 	"github.com/nspcc-dev/neo-go/pkg/rpc/client"
 	"github.com/nspcc-dev/neo-go/pkg/rpc/request"
 	"github.com/nspcc-dev/neo-go/pkg/smartcontract"
@@ -17,7 +20,7 @@ var (
 	contractAddress = "b85074ec25aa549814eceb2a4e3748f801c71c51"
 	contractUint, _ = util.Uint160DecodeStringBE(contractAddress)
 	wif             = ""
-	userWif         = "L2Dse3swNDZkwq2fkP5ctDMWB7x4kbvpkhzMJQ7oY9J2WBCATokR"
+	userWif         = "KyiLMuwnkwjNyuQJMmKvmFENCvC4rXAs9BdRSz9HTDmDFt93LRHt"
 	account, _      = wallet.NewAccountFromWIF(userWif)
 )
 
@@ -28,10 +31,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//pbs, err := hex.DecodeString("ee1445f89e85d41db71debefda3777ce4dbef9bc090fcde91bd906ae403c98c4")
+	//pbs, err := util.Uint256DecodeStringBE("42a854d2f9d7f01d4abf03bba5560dc91f3e88d5c71cab17f37872021b247a2d")
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
+	//r, err := c.GetRawTransaction(pbs)
+	//
+	//fmt.Println(r)
+	//fmt.Println(err)
+	//
+	//return
+
 	ps := []request.Param{
 		{
 			Type:  request.StringT,
@@ -45,27 +55,27 @@ func main() {
 						Type: smartcontract.ByteArrayType,
 						Value: request.Param{
 							Type:  request.StringT,
-							Value: "ee1445f89e85d41db71debefda3777ce4dbef9bc090fcde91bd906ae403c98c4",
+							Value: "8358c59912535612627221faab20ffdb99c3fb2e5074e7b75d5ea54799e2e4c7",
 						},
 					},
 				},
+				//{
+				//	Type: request.FuncParamT,
+				//	Value: request.FuncParam{
+				//		Type: smartcontract.PublicKeyType,
+				//		Value: request.Param{
+				//			Type:  request.StringT,
+				//			Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
+				//		},
+				//	},
+				//},
 				{
 					Type: request.FuncParamT,
 					Value: request.FuncParam{
 						Type: smartcontract.PublicKeyType,
 						Value: request.Param{
 							Type:  request.StringT,
-							Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
-						},
-					},
-				},
-				{
-					Type: request.FuncParamT,
-					Value: request.FuncParam{
-						Type: smartcontract.PublicKeyType,
-						Value: request.Param{
-							Type:  request.StringT,
-							Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
+							Value: "0276813a05c73a7458076b3c453c5f733362f7281b5a770f1ae35f3f63dec50a68",
 						},
 					},
 				},
@@ -75,7 +85,7 @@ func main() {
 						Type: smartcontract.IntegerType,
 						Value: request.Param{
 							Type:  request.NumberT,
-							Value: 10,
+							Value: 2,
 						},
 					},
 				},
@@ -85,7 +95,7 @@ func main() {
 						Type: smartcontract.PublicKeyType,
 						Value: request.Param{
 							Type:  request.StringT,
-							Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
+							Value: "0276813a05c73a7458076b3c453c5f733362f7281b5a770f1ae35f3f63dec50a68",
 						},
 					},
 				},
@@ -95,7 +105,7 @@ func main() {
 						Type: smartcontract.IntegerType,
 						Value: request.Param{
 							Type:  request.NumberT,
-							Value: 10,
+							Value: 17,
 						},
 					},
 				},
@@ -107,8 +117,21 @@ func main() {
 	if err != nil {
 		log.Fatal("script error: ", err)
 	}
-	tx := transaction.NewInvocationTX(scripts, 1)
-	log.Println(tx.Hash())
+
+	//re, err := c.SignAndPushInvocationTx(scripts, account, 0, 1)
+	//if err != nil {
+	//	log.Fatal("tx: ", err)
+	//}
+	//log.Println(re.String())
+
+	tx := transaction.NewInvocationTX(scripts, 0)
+	from, err := address.StringToUint160(account.Address)
+	if err != nil {
+		log.Fatal("sign error: ", err)
+	}
+	tx.AddVerificationHash(from)
+	bys, _ := json.Marshal(tx)
+	fmt.Println(string(bys))
 	err = account.SignTx(tx)
 	if err != nil {
 		log.Fatal("sign error: ", err)
@@ -117,4 +140,38 @@ func main() {
 	if err != nil {
 		log.Fatal("send error: ", err)
 	}
+	fmt.Println("==tx ", tx.Hash())
+}
+
+func invoke(client *client.Client) {
+	fmt.Println("==== invoke")
+	params := []smartcontract.Parameter{
+		{
+			Type:  smartcontract.ByteArrayType,
+			Value: "ee1445f89e85d41db71debefda3777ce4dbef9bc090fcde91bd906ae403c98c4",
+		},
+		{
+			Type:  smartcontract.PublicKeyType,
+			Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
+		},
+		{
+			Type:  smartcontract.PublicKeyType,
+			Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
+		},
+		{
+			Type:  smartcontract.IntegerType,
+			Value: 12,
+		},
+		{
+			Type:  smartcontract.PublicKeyType,
+			Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
+		},
+		{
+			Type:  smartcontract.IntegerType,
+			Value: 12,
+		},
+	}
+	r, err := client.InvokeFunction(contractAddress, "userLock", params, nil)
+	fmt.Println(r)
+	fmt.Println(err)
 }
