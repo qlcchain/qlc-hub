8f0dac51d61be62d9d1595fb9526d4a15bc65de7
zengchen221 <931891076@qq.com>
2020-08-27 09:05:45 +0800

    feat: test lock

--- pkg/neo/main/main.go
+++ pkg/neo/main/main.go
@@ -24,72 +24,95 @@ var (
 	url             = "http://seed2.ngd.network:20332"
 	contractAddress = "b85074ec25aa549814eceb2a4e3748f801c71c51"
 	contractUint, _ = util.Uint160DecodeStringLE(contractAddress)
-	wif             = ""
-	userWif         = "L2Dse3swNDZkwq2fkP5ctDMWB7x4kbvpkhzMJQ7oY9J2WBCATokR"
-	account, _      = wallet.NewAccountFromWIF(userWif)
-	from, _         = address.StringToUint160(account.Address)
+
+	userWif            = "L2Dse3swNDZkwq2fkP5ctDMWB7x4kbvpkhzMJQ7oY9J2WBCATokR"
+	userAccount, _     = wallet.NewAccountFromWIF(userWif)
+	userAccountUint, _ = address.StringToUint160(userAccount.Address)
+
+	wrapperWif            = "L2BAaQsPTDxGu1D9Q3x9ZS2ipabyzjBCNJAdP3D3NwZzL6KUqEkg"
+	wrapperAccount, _     = wallet.NewAccountFromWIF(wrapperWif)
+	wrapperAccountUint, _ = address.StringToUint160(wrapperAccount.Address)
 )
 
 func main() {
-	transaction2()
+	fmt.Println("user address    ==> ", userAccount.Address)
+	fmt.Println("wrapper address ==> ", wrapperAccount.Address)
+	rOrigin, rHash := hashValue()
+	fmt.Println("hash: ", rOrigin, "==>", rHash)
+
+	userLock(rHash)
+	//wrapperUnLock(rOrigin)
+
+	//userLockByPkg(rHash)
 }
 
-func transaction3() {
-
-}
-
-func transaction2() {
-	client, err := neo.NewNeoTransaction(url, contractAddress)
+func wrapperUnLock(rOrigin string) {
+	c, err := client.New(context.Background(), url, client.Options{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	s := String(10)
-	h := sha256.Sum256([]byte(s))
-	hash := hex.EncodeToString(h[:])
-	fmt.Println(s, "==>", hash)
 
-	params := []request.Param{
-		neo.FunctionName("userLock"),
-		neo.ArrayTypeParams([]request.Param{
-			neo.HashParam(hash),
-			neo.AddressParam(account.Address),
-			neo.IntegerTypeParam(210000000),
-			neo.AddressParam(account.Address),
-			neo.IntegerTypeParam(rand.Intn(100)),
-		}),
+	ps := []request.Param{
+		{
+			Type:  request.StringT,
+			Value: "wrapperUnlock",
+		}, {
+			Type: request.ArrayT,
+			Value: []request.Param{
+				{
+					Type: request.FuncParamT,
+					Value: request.FuncParam{
+						Type: smartcontract.StringType,
+						Value: request.Param{
+							Type:  request.StringT,
+							Value: rOrigin,
+						},
+					},
+				},
+				{
+					Type: request.FuncParamT,
+					Value: request.FuncParam{
+						Type: smartcontract.ByteArrayType,
+						Value: request.Param{
+							Type:  request.ArrayT,
+							Value: "ARNpaFJhp6SHziRomrK4cenWw66C8VVFyv",
+						},
+					},
+				},
+			},
+		},
 	}
-	r, err := client.CreateTransaction(params, userWif, 0, 0)
+
+	scripts, err := request.CreateFunctionInvocationScript(contractUint, ps)
 	if err != nil {
-		log.Fatal("tx error: ", err)
+		log.Fatal("script error: ", err)
 	}
-	log.Println(fmt.Sprintf("0x%s", r))
+	tx := transaction.NewInvocationTX(scripts, 0)
+	//tx.AddVerificationHash(userAccountUint)
+
+	err = wrapperAccount.SignTx(tx)
+
+	//tx.Scripts = append(tx.Scripts, transaction.Witness{
+	//	InvocationScript:   append([]byte{byte(opcode.PUSHBYTES64)}, sign...),
+	//	VerificationScript: a.getVerificationScript(),
+	//})
+
+	err = c.SendRawTransaction(tx)
+	if err != nil {
+		log.Fatal("send error: ", err)
+	}
+	fmt.Println("tx: ", fmt.Sprintf("0x%s", tx.Hash().StringLE()))
 }
 
-func transaction1() {
+func userLock(rHash string) {
 	c, err := client.New(context.Background(), url, client.Options{})
 	//c.SetWIF(userWif)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fromAddr := hex.EncodeToString(from.BytesBE())
-	fmt.Println(account.Address, "==>", fromAddr)
-
-	s := String(10)
-	h := sha256.Sum256([]byte(s))
-	hash := hex.EncodeToString(h[:])
-	fmt.Println(s, "==>", hash)
-
-	//pbs, err := util.Uint256DecodeStringBE("42a854d2f9d7f01d4abf03bba5560dc91f3e88d5c71cab17f37872021b247a2d")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//r, err := c.GetRawTransaction(pbs)
-	//
-	//fmt.Println(r)
-	//fmt.Println(err)
-	//
-	//return
+	fromAddr := hex.EncodeToString(userAccountUint.BytesBE())
+	fmt.Println(userAccount.Address, "==>", fromAddr)
 
 	ps := []request.Param{
 		{
@@ -104,7 +127,7 @@ func transaction1() {
 						Type: smartcontract.ByteArrayType,
 						Value: request.Param{
 							Type:  request.StringT,
-							Value: hash,
+							Value: rHash,
 						},
 					},
 				},
@@ -124,7 +147,7 @@ func transaction1() {
 						Type: smartcontract.IntegerType,
 						Value: request.Param{
 							Type:  request.NumberT,
-							Value: 290000000,
+							Value: 110000000,
 						},
 					},
 				},
@@ -164,10 +187,10 @@ func transaction1() {
 	//log.Println(re.String())
 
 	tx := transaction.NewInvocationTX(scripts, 0)
-	tx.AddVerificationHash(from)
+	tx.AddVerificationHash(userAccountUint)
 	bys, _ := json.MarshalIndent(tx, "", "\t")
 	fmt.Println(string(bys))
-	err = account.SignTx(tx)
+	err = userAccount.SignTx(tx)
 	if err != nil {
 		log.Fatal("sign error: ", err)
 	}
@@ -175,40 +198,30 @@ func transaction1() {
 	if err != nil {
 		log.Fatal("send error: ", err)
 	}
-	fmt.Println("==tx ", fmt.Sprintf("0x%s", tx.Hash().StringLE()))
+	fmt.Println("tx: ", fmt.Sprintf("0x%s", tx.Hash().StringLE()))
 }
 
-func invoke(client *client.Client) {
-	fmt.Println("==== invoke")
-	params := []smartcontract.Parameter{
-		{
-			Type:  smartcontract.ByteArrayType,
-			Value: "ee1445f89e85d41db71debefda3777ce4dbef9bc090fcde91bd906ae403c98c4",
-		},
-		{
-			Type:  smartcontract.PublicKeyType,
-			Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
-		},
-		{
-			Type:  smartcontract.PublicKeyType,
-			Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
-		},
-		{
-			Type:  smartcontract.IntegerType,
-			Value: 12,
-		},
-		{
-			Type:  smartcontract.PublicKeyType,
-			Value: "02bfc19e434bb9dde4be76adca4cb39d50bf9832a1ecd347e8a7f6c2bc01a0996f",
-		},
-		{
-			Type:  smartcontract.IntegerType,
-			Value: 12,
-		},
+func userLockByPkg(rHash string) {
+	client, err := neo.NewNeoTransaction(url, contractAddress)
+	if err != nil {
+		log.Fatal(err)
 	}
-	r, err := client.InvokeFunction(contractAddress, "userLock", params, nil)
-	fmt.Println(r)
-	fmt.Println(err)
+
+	params := []request.Param{
+		neo.FunctionName("userLock"),
+		neo.ArrayTypeParams([]request.Param{
+			neo.HashParam(rHash),
+			neo.AddressParam(userAccount.Address),
+			neo.IntegerTypeParam(120000000),
+			neo.AddressParam(userAccount.Address),
+			neo.IntegerTypeParam(rand.Intn(100)),
+		}),
+	}
+	r, err := client.CreateTransaction(params, userWif, 0, 0)
+	if err != nil {
+		log.Fatal("tx error: ", err)
+	}
+	log.Println(fmt.Sprintf("0x%s", r))
 }
 
 const charset = "abcdefghijklmnopqrstuvwxyz" +
@@ -227,4 +240,11 @@ func StringWithCharset(length int, charset string) string {
 
 func String(length int) string {
 	return StringWithCharset(length, charset)
+}
+
+func hashValue() (string, string) {
+	rOrigin := String(32)
+	h := sha256.Sum256([]byte(rOrigin))
+	rHash := hex.EncodeToString(h[:])
+	return rOrigin, rHash
 }
