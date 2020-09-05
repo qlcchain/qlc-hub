dbc80439afee8af197019cbbeea215c521eaf895
Goren G <yong.gu@qlink.mobi>
2020-09-05 16:13:51 +0800

    test: add sign test cases
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- signer/signer_test.go
+++ signer/signer_test.go
@@ -1,14 +1,21 @@
 package signer
 
 import (
+	"bytes"
+	"crypto/ecdsa"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+
+	"github.com/qlcchain/qlc-hub/pkg/util"
 
 	"github.com/qlcchain/qlc-hub/config"
 	"github.com/qlcchain/qlc-hub/grpc/proto"
 )
 
 func TestAuthClient_SignNeoTx(t *testing.T) {
-	//t.Skip()
+	t.Skip()
 	cfg := &config.Config{
 		Verbose:        true,
 		LogLevel:       "debug",
@@ -29,4 +36,33 @@ func TestAuthClient_SignNeoTx(t *testing.T) {
 	} else {
 		t.Fatal(err)
 	}
+
+	rawData := []byte(util.String(120))
+	neo := "1586faf62462d7b87c12c4c98ed28042b2bdfd715d3040baccdc267dfd60859a"
+	neoPriv, _ := keys.NewPrivateKeyFromHex(neo)
+	neoAddress := neoPriv.Address()
+	sign := neoPriv.Sign(rawData)
+	if neoResp, err := signer.Sign(proto.SignType_NEO, neoAddress, rawData); err == nil {
+		if !bytes.Equal(sign, neoResp.Sign) {
+			t.Fatalf("got: %v, exp: %v", neoResp.Sign, sign)
+		}
+	} else {
+		t.Fatal(err)
+	}
+
+	eth := "5d5f13593918431c70354607060d67e931a8bdc0575b4328e8ebb367b0d86d1d"
+	ethPriv, _ := crypto.HexToECDSA(eth)
+	publicKey := ethPriv.Public().(*ecdsa.PublicKey)
+	ethAddress := crypto.PubkeyToAddress(*publicKey).String()
+	h := crypto.Keccak256(rawData)
+	ethSign, _ := crypto.Sign(h, ethPriv)
+
+	if ethResp, err := signer.Sign(proto.SignType_ETH, ethAddress, h); err == nil {
+		if !bytes.Equal(ethSign, ethResp.Sign) {
+			t.Fatalf("got: %v, exp: %v", ethResp.Sign, sign)
+		}
+	} else {
+		t.Fatal(err)
+	}
+
 }
